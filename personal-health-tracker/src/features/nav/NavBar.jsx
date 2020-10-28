873d646f60366c59b204c31e76283695585aa6c4
ogbevireo <65548295+ogbevireo@users.noreply.github.com>
2020-10-28 15:43:03 +0100

    Update NavBar.jsx

--- personal-health-tracker/src/features/nav/NavBar.jsx
+++ personal-health-tracker/src/features/nav/NavBar.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { Container, Input, Menu, Select } from "semantic-ui-react";
-import { connect } from "react-redux";
-import { NavLink, Link } from "react-router-dom";
-import PropTypes from "prop-types";
-import { setActiveNavItem } from "../../app/redux/common/common.slice";
+import React, { useEffect, useState } from 'react';
+import { Container, Input, Menu, Select } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { NavLink, Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { setActiveNavItem } from '../../app/redux/common/common.slice';
 
 const NavBar = ({ setActiveNavItem, activeItem }) => {
   const [locationOptions, setLocationOptions] = useState([]);
@@ -12,11 +12,11 @@ const NavBar = ({ setActiveNavItem, activeItem }) => {
   useEffect(() => {
     // activeItem ? setActiveItem(activeItem) : setActiveItem('home');
     setLocationOptions([
-      { key: "lg", value: "lg", text: "Lagos" },
-      { key: "rv", value: "rv", text: "Rivers" },
-      { key: "ab", value: "ab", text: "Abuja" },
-      { key: "cr", value: "cr", text: "Cross Rivers" },
-      { key: "dl", value: "dl", text: "Delta" },
+      { key: 'lg', value: 'lg', text: 'Lagos' },
+      { key: 'rv', value: 'rv', text: 'Rivers' },
+      { key: 'ab', value: 'ab', text: 'Abuja' },
+      { key: 'cr', value: 'cr', text: 'Cross Rivers' },
+      { key: 'dl', value: 'dl', text: 'Delta' },
     ]);
     return () => {
       // cleanup;
@@ -24,36 +24,36 @@ const NavBar = ({ setActiveNavItem, activeItem }) => {
   }, [setActiveNavItem]);
 
   return (
-    <Menu fixed="top" secondary stackable size="mini" className="masthead-menu">
+    <Menu fixed='top' secondary stackable size='mini' className='masthead-menu'>
       <Container>
-        <Menu.Item header as={NavLink} exact to="/">
-          <img src="/assets/heart-beat-white.svg" alt="logo" />
+        <Menu.Item header as={NavLink} exact to='/'>
+          <img src='/assets/heart-beat-white.svg' alt='logo' />
         </Menu.Item>
         <Menu.Item>
-          <Input icon="search" placeholder="Find a doctor" />
+          <Input icon='search' placeholder='Find a doctor' />
         </Menu.Item>
         <Menu.Item>
-          <Select placeholder="Location" options={locationOptions} />
+          <Select placeholder='Location' options={locationOptions} />
         </Menu.Item>
 
-        <Menu.Menu position="right">
+        <Menu.Menu position='right'>
           <Menu.Item
-            name="LOGIN"
-            active={activeItem === "login"}
+            name='LOGIN'
+            active={activeItem === 'login'}
             onClick={handleItemClick}
             as={Link}
-            to="/Login"
+            to='/Login'
           />
           <Menu.Item
-            name="SIGNUP"
-            active={activeItem === "signup"}
+            name='SIGNUP'
+            active={activeItem === 'signup'}
             onClick={handleItemClick}
             as={Link}
-            to="/SignUp"
+            to='/SignUp'
           />
           <Menu.Item
-            name="CONTACT US"
-            active={activeItem === "contact us"}
+            name='CONTACT US'
+            active={activeItem === 'contact us'}
             onClick={handleItemClick}
           />
         </Menu.Menu>
