f4e9922dc2b3b867ce735d004eb0bdefb5ddfafd
ogbevireo <65548295+ogbevireo@users.noreply.github.com>
2020-10-28 15:53:10 +0100

    Update SignUp.jsx

--- personal-health-tracker/src/features/signup/SignUp.jsx
+++ personal-health-tracker/src/features/signup/SignUp.jsx
@@ -1,39 +1,39 @@
-import React from "react";
-import { Button, Container, Form, Grid, GridColumn } from "semantic-ui-react";
+import React from 'react';
+import { Button, Container, Form, Grid, GridColumn } from 'semantic-ui-react';
 
 const SignUp = () => (
   <Container>
     <Grid centered>
       <GridColumn style={{ maxWidth: 600, marginTop: 216 }}>
-        <Form class="ui huge form">
+        <Form class='ui huge form'>
           <Form.Field>
             <input
               fluid
-              type="email"
-              aria-label="email"
-              placeholder="Email"
+              type='email'
+              aria-label='email'
+              placeholder='Email'
               required
             />
           </Form.Field>
           <Form.Field>
             <input
               fluid
-              type="text"
-              aria-label="username"
-              placeholder="Username"
+              type='text'
+              aria-label='username'
+              placeholder='Username'
               required
             />
           </Form.Field>
           <Form.Field>
             <input
               fluid
-              type="password"
-              aria-label="password"
-              placeholder="Password"
+              type='password'
+              aria-label='password'
+              placeholder='Password'
               required
             />
           </Form.Field>
-          <Button fluid color="teal" type="submit">
+          <Button fluid color='teal' type='submit'>
             Sign Up
           </Button>
         </Form>
