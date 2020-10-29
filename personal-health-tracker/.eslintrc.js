509e9940b073ed2282d35a909805dabc81088acc
ogbevireo <ogbevire@gmail.com>
2020-10-29 16:14:25 +0100

    signup & login
    signup & login pages

--- personal-health-tracker/.eslintrc.js
+++ personal-health-tracker/.eslintrc.js
@@ -1,35 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2017": true,
-        "node": true
+  env: {
+    browser: true,
+    es2017: true,
+    node: true
+  },
+  extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": [
-        "plugin:react/recommended",
-        "plugin:prettier/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react", "prettier"
-    ],
-    "rules": {
-      "react/prop-types": 1,
-      "prettier/prettier": [
-        "error",
-        {
-          "printWidth": 80,
-          "semi": true,
-          "singleQuote": true,
-          "jsxSingleQuote": true,
-          "trailingComma": "none",
-          "endOfLine": "auto"
-        }
-      ]
-    }
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/prop-types': 1,
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        semi: true,
+        singleQuote: true,
+        jsxSingleQuote: true,
+        trailingComma: 'none',
+        endOfLine: 'auto'
+      }
+    ]
+  }
 };
